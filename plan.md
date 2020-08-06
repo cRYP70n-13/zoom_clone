f534a0d3d790fa28b1bd8965336676f45300b656
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-08-06 03:28:42 +0100

    adding a style.css file also started some really instersting shit and the app is starting to be amzing

--- plan.md
+++ plan.md
@@ -1,9 +1,9 @@
 # plan of action
 
-- Initialize the project
-- Initialize the first view
-- Create a room ID
-- Add the ability to view our video
+- Initialize the project [x]
+- Initialize the first view [x]
+- Create a room ID [x]
+- Add the ability to view our video [x]
 - Add the ability to let others stream
 - Add styling
 - Add the ability to create messages
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const dotenv = require('dotenv');
-const { v4: uuidv4 } = require('uuid')
+const dotenv = require("dotenv");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 // Load env variables
 dotenv.config({ path: './config/config.env' });
-
-const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -20,4 +22,11 @@ app.get('/:room', (req, res) => {
 	})
 });
 
-app.listen(5000, () => console.log(`this shit is up and running`))
+io.on('connection', socket => {
+	socket.on('join-room', (roomId) => {
+		socket.join(roomId);
+		socket.to(roomId).broadcast.emit('user-connected');
+	})
+})
+
+server.listen(5000, () => console.log(`this shit is up and running`))
