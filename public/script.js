ef8290f0122e4ef375963d0f345819a562c6dd4b
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-08-07 00:34:19 +0100

    updating all the double quotes to singles quotes using vim

--- public/script.js
+++ public/script.js
@@ -1,14 +1,17 @@
-const socket = io("/");
-const videoGrid = document.getElementById("video-grid");
+const socket = io('/');
+const videoGrid = document.getElementById('video-grid');
+
 const myPeer = new Peer(undefined, {
-  path: "/peerjs",
-  host: "/",
-  port: "5000",
+  path: '/peerjs',
+  host: '/',
+  port: '5000',
 });
+
 let myVideoStream;
-const myVideo = document.createElement("video");
+const myVideo = document.createElement('video');
 myVideo.muted = true;
 const peers = {};
+
 navigator.mediaDevices
   .getUserMedia({
     video: true,
@@ -17,47 +20,47 @@ navigator.mediaDevices
   .then((stream) => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
-    myPeer.on("call", (call) => {
+    myPeer.on('call', (call) => {
       call.answer(stream);
-      const video = document.createElement("video");
-      call.on("stream", (userVideoStream) => {
+      const video = document.createElement('video');
+      call.on('stream', (userVideoStream) => {
         addVideoStream(video, userVideoStream);
       });
     });
 
-    socket.on("user-connected", (userId) => {
+    socket.on('user-connected', (userId) => {
       connectToNewUser(userId, stream);
     });
     // input value
-    let text = $("input");
+    let text = $('input');
     // when press enter send message
-    $("html").keydown(function (e) {
+    $('html').keydown(function (e) {
       if (e.which == 13 && text.val().length !== 0) {
-        socket.emit("message", text.val());
-        text.val("");
+        socket.emit('message', text.val());
+        text.val('');
       }
     });
-    socket.on("createMessage", (message) => {
-      $("ul").append(`<li class="message"><b>user</b><br/>${message}</li>`);
+    socket.on('createMessage', (message) => {
+      $('ul').append(`<li class='message'><b>user</b><br/>${message}</li>`);
       scrollToBottom();
     });
   });
 
-socket.on("user-disconnected", (userId) => {
+socket.on('user-disconnected', (userId) => {
   if (peers[userId]) peers[userId].close();
 });
 
-myPeer.on("open", (id) => {
-  socket.emit("join-room", ROOM_ID, id);
+myPeer.on('open', (id) => {
+  socket.emit('join-room', ROOM_ID, id);
 });
 
 function connectToNewUser(userId, stream) {
   const call = myPeer.call(userId, stream);
-  const video = document.createElement("video");
-  call.on("stream", (userVideoStream) => {
+  const video = document.createElement('video');
+  call.on('stream', (userVideoStream) => {
     addVideoStream(video, userVideoStream);
   });
-  call.on("close", () => {
+  call.on('close', () => {
     video.remove();
   });
 
@@ -66,15 +69,15 @@ function connectToNewUser(userId, stream) {
 
 function addVideoStream(video, stream) {
   video.srcObject = stream;
-  video.addEventListener("loadedmetadata", () => {
+  video.addEventListener('loadedmetadata', () => {
     video.play();
   });
   videoGrid.append(video);
 }
 
 const scrollToBottom = () => {
-  var d = $(".main__chat_window");
-  d.scrollTop(d.prop("scrollHeight"));
+  var d = $('.main__chat_window');
+  d.scrollTop(d.prop('scrollHeight'));
 };
 
 const muteUnmute = () => {
@@ -89,7 +92,7 @@ const muteUnmute = () => {
 };
 
 const playStop = () => {
-  console.log("object");
+  console.log('object');
   let enabled = myVideoStream.getVideoTracks()[0].enabled;
   if (enabled) {
     myVideoStream.getVideoTracks()[0].enabled = false;
@@ -102,32 +105,32 @@ const playStop = () => {
 
 const setMuteButton = () => {
   const html = `
-    <i class="fas fa-microphone"></i>
+    <i class='fas fa-microphone'></i>
     <span>Mute</span>
   `;
-  document.querySelector(".main__mute_button").innerHTML = html;
+  document.querySelector('.main__mute_button').innerHTML = html;
 };
 
 const setUnmuteButton = () => {
   const html = `
-    <i class="unmute fas fa-microphone-slash"></i>
+    <i class='unmute fas fa-microphone-slash'></i>
     <span>Unmute</span>
   `;
-  document.querySelector(".main__mute_button").innerHTML = html;
+  document.querySelector('.main__mute_button').innerHTML = html;
 };
 
 const setStopVideo = () => {
   const html = `
-    <i class="fas fa-video"></i>
+    <i class='fas fa-video'></i>
     <span>Stop Video</span>
   `;
-  document.querySelector(".main__video_button").innerHTML = html;
+  document.querySelector('.main__video_button').innerHTML = html;
 };
 
 const setPlayVideo = () => {
   const html = `
-  <i class="stop fas fa-video-slash"></i>
+  <i class='stop fas fa-video-slash'></i>
     <span>Play Video</span>
   `;
-  document.querySelector(".main__video_button").innerHTML = html;
+  document.querySelector('.main__video_button').innerHTML = html;
 };
