367a2d59efafcb5904e1111989495b701e7d8376
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-08-06 02:58:43 +0100

    adding some new functionnalities and the video now is up and running as I want keep it up body

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const { v4: uuidv4 } = require('uuid')
 
 // Load env variables
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-	res.render('room');
-})
+	res.redirect(`/${uuidv4()}`);
+});
+
+app.get('/:room', (req, res) => {
+	res.render('room', {
+		roomId: req.params.room
+	})
+});
 
 app.listen(5000, () => console.log(`this shit is up and running`))
